fca252df22ae9959c54c5a3792faa0f67ef264ef
yasakova-anastasia <anastasia.yasakova@intel.com>
2020-10-23 16:14:56 +0300

    add test

--- tests/test_imagenet_format.py
+++ tests/test_imagenet_format.py
@@ -71,6 +71,47 @@ class ImagenetFormatTest(TestCase):
 
             compare_datasets(self, source_dataset, parsed_dataset)
 
+    def test_can_save_and_load_with_multiple_labels_(self):
+        source_dataset = Dataset.from_iterable([
+            DatasetItem(id='1',
+                image=np.ones((8, 8, 3)),
+                annotations=[Label(0), Label(1)]
+            ),
+            DatasetItem(id='2',
+                image=np.ones((10, 10, 3)),
+                annotations=[Label(0), Label(1)]
+            ),
+            DatasetItem(id='3',
+                image=np.ones((10, 10, 3)),
+                annotations=[Label(0), Label(2)]
+            ),
+            DatasetItem(id='4',
+                image=np.ones((8, 8, 3)),
+                annotations=[Label(2), Label(4)]
+            ),
+            DatasetItem(id='5',
+                image=np.ones((10, 10, 3)),
+                annotations=[Label(3), Label(4)]
+            ),
+            DatasetItem(id='6',
+                image=np.ones((10, 10, 3)),
+            ),
+            DatasetItem(id='7',
+                image=np.ones((8, 8, 3))
+            ),
+        ], categories={
+            AnnotationType.label: LabelCategories.from_iterable(
+                'label_' + str(label) for label in range(5)),
+        })
+
+        with TestDir() as test_dir:
+            ImagenetConverter.convert(source_dataset, test_dir, save_images=True)
+
+            parsed_dataset = ImagenetImporter()(test_dir).make_dataset()
+
+            compare_datasets(self, source_dataset, parsed_dataset)
+
+
 DUMMY_DATASET_DIR = osp.join(osp.dirname(__file__), 'assets', 'imagenet_dataset')
 
 class ImagenetImporterTest(TestCase):
